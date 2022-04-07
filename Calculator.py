56fc81c08935dcb49462075275f46a7c4c907c25
Naman Gathani <58263196+namangathani@users.noreply.github.com>
2022-04-08 00:20:13 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,29 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+
+while True:
+    choice = input("Enter choice(1/2): ")
+
+    if choice in ('1', '2'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
+    else:
+        print("Invalid Input")
