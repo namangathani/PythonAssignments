bfca9a6169f730b6badc3b37100e191fc90b1cfd
Naman Gathani <58263196+namangathani@users.noreply.github.com>
2022-04-08 00:20:30 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,14 @@
+rows_no = 5
+for i in range(1, rows_no + 1):
+    for k in range(1, i):
+        print(" ", end = "")
+    for j in range(i, rows_no + 1):
+        print("* ", end = " ")
+ 
+    print()
+for i in range(rows_no - 1, 0, -1):
+    for k in range(1, i):
+        print(" ", end = "")
+    for j in range(i, rows_no + 1):
+        print("* ", end = " ")
+    print()
